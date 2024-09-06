448911c848fdaf6812350d9da05c8978b2b010d4
Jarosław <jarekigras@gmail.com>
2024-09-06 18:44:48 +0200

    Update fiile.py

--- fiile.py
+++ fiile.py
@@ -1,51 +1,16 @@
-import csv
-from collections import defaultdict
+def expand_dict_column(df, column_name):
+    # Rozszerzamy słowniki na osobne kolumny
+    expanded_df = df[column_name].apply(pd.Series)
+    # Renomujemy kolumny, aby uwzględnić nazwę kolumny bazowej
+    expanded_df.columns = [f"{column_name}.{col}" for col in expanded_df.columns]
+    return expanded_df
 
+# Rozszerzamy kolumny block1 i block2
+expanded_block1 = expand_dict_column(df, 'block1')
+expanded_block2 = expand_dict_column(df, 'block2')
 
+# Łączymy z powrotem z oryginalnym DataFrame
+df_expanded = pd.concat([df, expanded_block1, expanded_block2], axis=1)
 
-def flatten_data_for_all_keys(data):
-    result = defaultdict(lambda: defaultdict(list))
-
-    for key, value in data.items():
-        for field in value["field"]:
-            field_name = field[0]
-            field_data = field[1]
-
-            if 'item' in field_data and field_data['item']:
-                item = field_data['item'][0]
-                result[field_name]['item.raw'].append(item.get('raw', ''))
-
-                if 'component' in item:
-                    for comp in item['component']:
-                        if len(comp) == 2:  #
-                            comp_name, comp_value = comp
-                            result[field_name][f'item.component.{comp_name}'].append(comp_value)
-
-    return result
-
-
-
-flat_data = flatten_data_for_all_keys(data)
-
-
-# Przetwórz dane i zapisz do plików CSV
-def save_to_txt(data_dict, filename):
-    headers = list(data_dict.keys())
-    num_rows = max(len(data_dict[header]) for header in headers)
-
-    with open(filename, 'w') as txtfile:
-
-        header_line = ','.join(headers) + '\n'
-        txtfile.write(header_line)
-
-
-        for i in range(num_rows):
-            row = [data_dict[header][i] if i < len(data_dict[header]) else '' for header in headers]
-            row_line = ','.join(map(str, row)) + '\n'
-            txtfile.write(row_line)
-
-
-for key, data_dict in flat_data.items():
-    filename = f'{key}.txt'
-    save_to_txt(data_dict, filename)
-    print(f'TXT file created: {filename}')
+# Usuwamy kolumny z oryginalnymi słownikami (opcjonalne)
+df_expanded = df_expanded.drop(columns=['block1', 'block2'])
