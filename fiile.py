b5d2015b903256c243cac21dafc7ce92bf41096e
Jarosław <jarekigras@gmail.com>
2024-09-06 18:55:01 +0200

    Update fiile.py

--- fiile.py
+++ fiile.py
@@ -1,16 +1,24 @@
-def expand_dict_column(df, column_name):
-    # Rozszerzamy słowniki na osobne kolumny
-    expanded_df = df[column_name].apply(pd.Series)
-    # Renomujemy kolumny, aby uwzględnić nazwę kolumny bazowej
-    expanded_df.columns = [f"{column_name}.{col}" for col in expanded_df.columns]
-    return expanded_df
+def save_to_csv(data_dict, filename):
+    # Pobierz nagłówki (klucze z wewnętrznego słownika)
+    headers = list(data_dict.keys())
+    
+    # Sprawdź długość najdłuższej listy, aby ustalić liczbę wierszy
+    num_rows = max(len(data_dict[header]) for header in headers)
+    
+    # Otwórz plik w trybie zapisu
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        
+        # Zapisz nagłówki (jako nagłówki kolumn)
+        writer.writerow(headers)
+        
+        # Zapisz wartości wierszy
+        for i in range(num_rows):
+            row = [data_dict[header][i] if i < len(data_dict[header]) else '' for header in headers]
+            writer.writerow(row)
 
-# Rozszerzamy kolumny block1 i block2
-expanded_block1 = expand_dict_column(df, 'block1')
-expanded_block2 = expand_dict_column(df, 'block2')
-
-# Łączymy z powrotem z oryginalnym DataFrame
-df_expanded = pd.concat([df, expanded_block1, expanded_block2], axis=1)
-
-# Usuwamy kolumny z oryginalnymi słownikami (opcjonalne)
-df_expanded = df_expanded.drop(columns=['block1', 'block2'])
+# Przetwórz dane i zapisz do plików CSV z rozszerzeniem .txt
+for key, data_dict in data.items():
+    filename = f'{key}.txt'
+    save_to_csv(data_dict, filename)
+    print(f'TXT file created: {filename}')
