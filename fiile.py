866919c7811608032ae404d12a6a76f78bda8a6a
Jarosław <jarekigras@gmail.com>
2024-09-06 17:44:46 +0200

    Update fiile.py

--- fiile.py
+++ fiile.py
@@ -1,35 +1,39 @@
-def flatten_data_for_key(data, key):
-    flat_dict = {}
-    
-    for field in data[key]["field"]:
-        field_name = field[0]
-        field_data = field[1]
-        
-        if 'item' in field_data:
-            item = field_data['item'][0]
+def flatten_data_for_all_keys(data):
+    result = defaultdict(lambda: defaultdict(list))
+
+    for key, value in data.items():
+        for field in value["field"]:
+            field_name = field[0]
+            field_data = field[1]
             
-            # Przypisz raw value
-            flat_dict[f'{field_name}.item.raw'] = item['raw']
-            
-            # Przypisz component values
-            if 'component' in item:
-                for comp in item['component']:
-                    comp_name, comp_value = comp
-                    flat_dict[f'{field_name}.item.component.{comp_name}'] = comp_value
+            if 'item' in field_data:
+                item = field_data['item'][0]
+                
+                # Dodaj raw value
+                result[field_name]['item.raw'].append(item['raw'])
+                
+                # Dodaj component values
+                if 'component' in item:
+                    for comp in item['component']:
+                        comp_name, comp_value = comp
+                        result[field_name][f'item.component.{comp_name}'].append(comp_value)
     
-    return flat_dict
+    return result
 
 # Funkcja do zapisywania danych do pliku CSV
 def save_to_csv(flat_data, filename):
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Key', 'Value'])
-        for key, value in flat_data.items():
-            writer.writerow([key, value])
+        for key, values in flat_data.items():
+            for sub_key, sub_values in values.items():
+                for value in sub_values:
+                    writer.writerow([f'{key}.{sub_key}', value])
 
 # Przetwórz dane i zapisz do plików CSV
-for key in data:
-    flat_data = flatten_data_for_key(data, key)
-    filename = f'output_{key}.csv'
-    save_to_csv(flat_data, filename)
+flat_data = flatten_data_for_all_keys(data)
+
+for field_name, data_dict in flat_data.items():
+    filename = f'{field_name}.csv'
+    save_to_csv(data_dict, filename)
     print(f'CSV file created: {filename}')
