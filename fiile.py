2697c07ae9fabdca64532afdf365f47fdcc2ac15
Jarosław <jarekigras@gmail.com>
2024-09-06 17:36:26 +0200

    Update fiile.py

--- fiile.py
+++ fiile.py
@@ -1,37 +1,24 @@
-def flatten_for_key(nested_json, target_key='50k', parent_key='', sep='.'):
-    items = []
+def flatten_data(data):
+    flat_dict = {}
     
-    if isinstance(nested_json, list):
-        for element in nested_json:
-            if isinstance(element, list) and len(element) == 2:
-                key, value = element
-                if key == target_key:
-                    new_key = f"{parent_key}{sep}{target_key}" if parent_key else target_key
-                    items.extend(flatten_json(value, new_key, sep=sep).items())
+    for key, value in data.items():
+        for field in value["field"]:
+            field_name = field[0]
+            field_data = field[1]
+            
+            if 'item' in field_data:
+                item = field_data['item'][0]
+                
+                # Przypisz raw value
+                flat_dict[f'{field_name}.item.raw'] = item['raw']
+                
+                # Przypisz component values
+                if 'component' in item:
+                    for comp in item['component']:
+                        comp_name, comp_value = comp
+                        flat_dict[f'{field_name}.item.component.{comp_name}'] = int(comp_value)
     
-    return dict(items)
+    return flat_dict
 
-def flatten_json(nested_json, parent_key='', sep='.'):
-    items = []
-    
-    if isinstance(nested_json, dict):
-        for k, v in nested_json.items():
-            new_key = f"{parent_key}{sep}{k}" if parent_key else k
-            if isinstance(v, dict):
-                items.extend(flatten_json(v, new_key, sep=sep).items())
-            elif isinstance(v, list):
-                if all(isinstance(i, dict) for i in v):
-                    for item in v:
-                        items.extend(flatten_json(item, new_key, sep=sep).items())
-                elif all(isinstance(i, list) for i in v):
-                    for comp in v:
-                        comp_key = f"{new_key}.component.{comp[0]}"
-                        items.append((comp_key, comp[1]))
-            else:
-                items.append((new_key, v))
-    
-    return dict(items)
-
-flattened_result = flatten_for_key(data["0"]["field"], target_key='50k')
-
-print(flattened_result)
+# Generowanie płaskiego słownika
+flat_data = flatten_data(data)
