70cb36a41f5eb8b7c722b21ed4e432e78d9adb01
Jarosław <jarekigras@gmail.com>
2024-09-06 17:14:17 +0200

    Update fiile.py

--- fiile.py
+++ fiile.py
@@ -1,47 +1,33 @@
-def flatten_json(nested_json, parent_key='', sep='.'):
+def flatten_for_key(nested_json, target_key='50k', parent_key='', sep='.'):
     items = []
     
     if isinstance(nested_json, list):
-        # Obsługa listy, np. [["50k": {...}], ["4k": {...}]]
-        for idx, element in enumerate(nested_json):
+        for element in nested_json:
             if isinstance(element, list):
                 for sub_element in element:
-                    for k, v in sub_element.items():
-                        new_key = f"{parent_key}{sep}{k}" if parent_key else k
-                        if isinstance(v, dict):
-                            items.extend(flatten_json(v, new_key, sep=sep).items())
-            else:
-                items.extend(flatten_json(element, parent_key, sep=sep).items())
+                    if target_key in sub_element:
+                        new_key = f"{parent_key}{sep}{target_key}" if parent_key else target_key
+                        items.extend(flatten_json(sub_element[target_key], new_key, sep=sep).items())
     
-    elif isinstance(nested_json, dict):
-        # Obsługa słownika
+    return dict(items)
+
+def flatten_json(nested_json, parent_key='', sep='.'):
+    items = []
+    
+    if isinstance(nested_json, dict):
         for k, v in nested_json.items():
             new_key = f"{parent_key}{sep}{k}" if parent_key else k
             if isinstance(v, dict):
                 items.extend(flatten_json(v, new_key, sep=sep).items())
             elif isinstance(v, list) and all(isinstance(i, list) for i in v):
-                # Obsługa list komponentów
                 for comp in v:
                     comp_key = f"{new_key}.component.{comp[0]}"
                     items.append((comp_key, comp[1]))
             else:
                 items.append((new_key, v))
-    else:
-        # Obsługa wartości końcowych (liści)
-        items.append((parent_key, nested_json))
     
     return dict(items)
 
-# Iteracja przez dane JSON i spłaszczenie struktury
-flattened_keys = set()  # Zbiór, aby zebrać wszystkie unikalne klucze
+flattened_result = flatten_for_key(data[0]["field"], target_key='50k')
 
-for idx, entry in data.items():
-    for field in entry["field"]:
-        flattened = flatten_json(field)  # Spłaszczamy każdy element w liście "field"
-        flattened_keys.update(flattened.keys())  # Dodajemy klucze do zbioru
-
-# Konwersja zbioru kluczy na posortowaną listę, aby stworzyć nagłówki
-headers = sorted(flattened_keys)
-
-# Wyświetlamy automatycznie wygenerowane nagłówki
-print(headers)
+print(flattened_result)
