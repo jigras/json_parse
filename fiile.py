84d46483338053c6ed240eec30f94bfa656f3af0
Jarosław <jarekigras@gmail.com>
2024-09-06 17:42:16 +0200

    Update fiile.py

--- fiile.py
+++ fiile.py
@@ -1,24 +1,35 @@
-def flatten_data(data):
+def flatten_data_for_key(data, key):
     flat_dict = {}
     
-    for key, value in data.items():
-        for field in value["field"]:
-            field_name = field[0]
-            field_data = field[1]
+    for field in data[key]["field"]:
+        field_name = field[0]
+        field_data = field[1]
+        
+        if 'item' in field_data:
+            item = field_data['item'][0]
             
-            if 'item' in field_data:
-                item = field_data['item'][0]
-                
-                # Przypisz raw value
-                flat_dict[f'{field_name}.item.raw'] = item['raw']
-                
-                # Przypisz component values
-                if 'component' in item:
-                    for comp in item['component']:
-                        comp_name, comp_value = comp
-                        flat_dict[f'{field_name}.item.component.{comp_name}'] = int(comp_value)
+            # Przypisz raw value
+            flat_dict[f'{field_name}.item.raw'] = item['raw']
+            
+            # Przypisz component values
+            if 'component' in item:
+                for comp in item['component']:
+                    comp_name, comp_value = comp
+                    flat_dict[f'{field_name}.item.component.{comp_name}'] = comp_value
     
     return flat_dict
 
-# Generowanie płaskiego słownika
-flat_data = flatten_data(data)
+# Funkcja do zapisywania danych do pliku CSV
+def save_to_csv(flat_data, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Key', 'Value'])
+        for key, value in flat_data.items():
+            writer.writerow([key, value])
+
+# Przetwórz dane i zapisz do plików CSV
+for key in data:
+    flat_data = flatten_data_for_key(data, key)
+    filename = f'output_{key}.csv'
+    save_to_csv(flat_data, filename)
+    print(f'CSV file created: {filename}')
